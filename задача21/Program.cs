050f3c30bbc5fe7507c2a9125e617a5e72af2ba7
Lena <imagine.youeself19@gmail.com>
2022-06-30 11:49:42 +0300

    добавлена задача 21

---
+++ задача21/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите значения координат двух точек в порядке: x, y, z, x1, y1, z1");
+double x, y, x1, y1, z, z1, len;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+z = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+z1 = Convert.ToDouble(Console.ReadLine());
+len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1));
+Console.WriteLine($"Расстояние между этими точками в 3D пространстве равно {len}");
