92a80986457ec275f061517fd605d4f08210a3d3
Lena <imagine.youeself19@gmail.com>
2022-06-30 11:56:09 +0300

    добавлена задача 23

---
+++ задача23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число для выдачи таблицы кубов чисел от 1 до n");
+int n;
+n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++){
+    Console.WriteLine(i * i * i);
+}
