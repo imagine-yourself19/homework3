560755568cb8364894b82ebc854fec50a1700a91
Lena <imagine.youeself19@gmail.com>
2022-07-04 10:16:42 +0300

    добавлена задача19

--- задача19/Program.cs
+++ задача19/Program.cs
@@ -1,13 +1,13 @@
-﻿int num, n1, n2, n4, n5;
-Console.WriteLine("Введите число для проверки, является ли оно палиндромом");
-num = Convert.ToInt32(Console.ReadLine());
-n1 = num / 10000;
-n2 = num / 1000;
-n4 = num / 10;
-n5 = num % 10;
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int n1, n2, n4, n5;
+n1 = n / 10000;
+n2 = (n % 10000) / 1000;
+
+n4 = (n % 100) / 10;
+n5 = n % 10;
 if (n1 == n5 && n2 == n4){
-    Console.WriteLine("palindrome");
+    Console.WriteLine("Данное число является палиндромом");
 }
-else {
-    Console.WriteLine("isn't palindrome");
+else{
+    Console.WriteLine("Не является палиндромом");
 }
